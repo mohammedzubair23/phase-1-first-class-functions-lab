70228172f954a40c4b5c6938a3215f014f7c30e2
Mohammed Zubair <mohammedzubair10@yahoo.com>
2022-01-11 17:11:50 -0500

    commit tree 42659ce6f424a2eedf42e19a85ce648f977386d7

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+  };
+
+  const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+  };
+
+  const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
+
+  function createFareMultiplier(num){
+      return function (number){
+         return number * num
+      }
+  }
+
+
+  function fareTripler(num){
+    return num * 3
+  }
+
+  function fareDoubler(num){
+    return num * 2
+  }
+
+  function fetchSpecifiedDrivers(array, func){
+    return func(array)
+  }
+  
+  const selectDifferentDrivers = function (drivers, whichDrivers) {
+    return whichDrivers(drivers);
+  }
+ 
+  
+  
